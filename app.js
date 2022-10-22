1a06951450fab50d75b50360cd92af59246d0acd
eoghankb <43074374+eoghankbdev@users.noreply.github.com>
2022-10-22 06:11:39 +0100

    Added app.js file

---
+++ app.js
@@ -0,0 +1,8 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorALL('.controlls');
+const sectBtn = document.querySelectorAll('.control');
+
+function PageTransitions(){
+    //button click active class
+    
+}

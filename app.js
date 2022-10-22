92c789249b7f73dfffb6ecc97042518a0d283b86
eoghankb <43074374+eoghankbdev@users.noreply.github.com>
2022-10-22 07:27:58 +0100

    Added active page transitions

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const sections = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorALL('.controlls');
+const sectBtns = document.querySelectorAll('.controlls');
 const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
 
-function PageTransitions(){
+function PageTransitions() {
     //button click active class
-    
+    for(let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function(){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
+        })
+    }
 }
+
+PageTransitions();
